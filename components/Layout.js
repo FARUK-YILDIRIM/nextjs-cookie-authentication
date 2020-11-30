64195c5ea62e3d4a2ab325341f562941af9c8a47
frkyldrm <faruk.yildirim@outlook.com.tr>
2020-11-30 18:00:54 +0300

    user session cliend and server

--- components/Layout.js
+++ components/Layout.js
@@ -1,53 +1,64 @@
 import Link from "next/link";
+import React from "react";
 
-const Layout = ({ title, children }) => (
-  <div className="root">
-    <span>
-      Welcome, <strong>Guest</strong>
-    </span>
-    <nav className="navbar">
-      <div>
-        <Link href="/">
-          <a>Home</a>
-        </Link>
-        <Link href="/profile">
-          <a>Profile</a>
-        </Link>
-        <button>Logout</button>
-        <Link href="/login">
-          <a>Login</a>
-        </Link>
-      </div>
-    </nav>
-    <h1>{title}</h1>
-    {children}
+const Layout = ({ title, children, auth }) => {
+  const { user = {} } = auth || {};
+  return (
+    <div className="root">
+      <span>
+        Welcome, <strong>{user.name || "Guest"}</strong>
+      </span>
+      <nav className="navbar">
+        <div>
+          <Link href="/">
+            <a>Home</a>
+          </Link>
+          {user.email ? (
+            //Auth Navigation
+            <React.Fragment>
+              <Link href="/profile">
+                <a>Profile</a>
+              </Link>
+              <button>Logout</button>
+            </React.Fragment>
+          ) : (
+            // UnAuth Navigation
+            <Link href="/login">
+              <a>Login</a>
+            </Link>
+          )}
+        </div>
+      </nav>
+      <h1>{title}</h1>
+      {children}
 
-    <style jsx>
-      {`
-        .root {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-direction: column;
-        }
-        .navbar {
-          with: 100%;
-          display: flex;
-          justify-content: space-around;
-        }
-        a {
-          margin-right: 0.5em;
-        }
-        button {
-          text-decoration: 0;
-          padding: 0;
-          font: inherit;
-          border-style: none;
-          color: rgb(0, 0, 238);
-        }
-      `}
-    </style>
-  </div>
-);
+      <style jsx>
+        {`
+          .root {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            flex-direction: column;
+          }
+          .navbar {
+            with: 100%;
+            display: flex;
+            justify-content: space-around;
+          }
+          a {
+            margin-right: 0.5em;
+          }
+          button {
+            text-decoration: 0;
+            padding: 0;
+            font: inherit;
+            border-style: none;
+            color: rgb(0, 0, 238);
+          }
+        `}
+      </style>
+    </div>
+  );
+};
 
 export default Layout;
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
 import Layout from "../components/Layout";
 import Link from "next/link";
-
-export default function Index() {
+import { authInitialProps } from "../lib/auth";
+export default function Index(props) {
   return (
-    <Layout title="Home">
+    <Layout title="Home" {...props}>
       <Link href="/profile">
         <a>go to profile</a>
       </Link>
     </Layout>
   );
 }
+
+Index.getInitialProps = authInitialProps();
